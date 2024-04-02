07087b9725d7fc4fdfc3255e08118d5c0c1465d9
surungo <surungo@gmail.com>
2024-04-01 21:55:36 -0300

    1.1.0 edit add decrease

--- src/app/storage/balance.storage.ts
+++ src/app/storage/balance.storage.ts
@@ -17,7 +17,11 @@ export class BalanceStorage {
     public balance_dataSource = signal(new MatTableDataSource(this.balance_dataToDisplay()));
     public balance_selection = signal(new SelectionModel<Balance>(true, []));
 
-    public balanceName = signal("");
+    public idbalance = signal(0);
+    public name = signal("");
+    public amount = signal(0);
+    public free = signal(false);
+    
     public balanceLength = signal(0);
     public totalAmount = signal(0);
     public totalNoFree = signal(0);
@@ -34,7 +38,26 @@ export class BalanceStorage {
         this.balance_dataSource.set(new MatTableDataSource(this.balance_dataToDisplay()));
         return this.balance_dataToDisplay();
     }
+    decreaseData(value: Balance) {
+        this.balance.set(value);
+        if(this.balance().idbalance>0){
+            this.balance_dataToDisplay.set(this.storageService.get(this.nmDataBalance));
+            this.balance.set(this.validName(this.balance()));
+            this.balance_dataToDisplay().forEach((value, index) => {
+                if (value.idbalance == this.balance().idbalance){
+                value.idbalance=this.balance().idbalance;
+                value.name=this.balance().name;
+                value.amount=value.amount-this.balance().amount;
+                value.free=this.balance().free;
+                }
+            });
+            this.balance_dataSource().data = this.balance_dataToDisplay();
+            this.balance_selection().clear();
+            this.storageService.set(this.nmDataBalance, this.balance_dataToDisplay());
+            this.loadBalanceData();
 
+        }
+    }
     addData(value: Balance) {
         let count = 1;
         let idbalance = 0;
@@ -42,30 +65,55 @@ export class BalanceStorage {
 
         // load Storage
         this.loadBalanceData();
+        if(this.balance().idbalance>0){
+            this.balance_dataToDisplay.set(this.storageService.get(this.nmDataBalance));
+            this.balance.set(this.validName(this.balance()));
+            this.balance_dataToDisplay().forEach((value, index) => {
+                if (value.idbalance == this.balance().idbalance){
+                  value.idbalance=this.balance().idbalance;
+                  value.name=this.balance().name;
+                  value.amount+=this.balance().amount;
+                  value.free=this.balance().free;
+                }
+            });
+            this.balance_dataSource().data = this.balance_dataToDisplay();
+            this.balance_selection().clear();
+            this.storageService.set(this.nmDataBalance, this.balance_dataToDisplay());
+            this.loadBalanceData();
 
-        // select balance id
-        if (this.storageService.get(this.nmDataBalance).length != 0) {
-            while (idbalance == 0) {
-                idbalance = count;
-                this.balance_dataToDisplay().forEach((value, index) => {
-                    if (value.idbalance == count) {
-                        idbalance = 0;
-                    }
-                });
-                count++;
+        }else{
+            // select balance id
+            if (this.storageService.get(this.nmDataBalance).length != 0) {
+                while (idbalance == 0) {
+                    idbalance = count;
+                    this.balance_dataToDisplay().forEach((value, index) => {
+                        if (value.idbalance == count) {
+                            idbalance = 0;
+                        }
+                    });
+                    count++;
+                }
+                this.balance().idbalance = idbalance;
+            } else {
+                this.balance().idbalance = 1;
             }
-            this.balance().idbalance = idbalance;
-        } else {
-            this.balance().idbalance = 1;
+
+            this.balance.set(this.validName(this.balance()));
+            
+            // add new balance storage
+            this.addBalance(this.balance());
         }
+    }
+
+    validName(balance: Balance): Balance{
         this.balance_dataToDisplay().forEach((value, index) => {
-            if (value.name == this.balance().name) {
-                this.balance().name = this.balance().name + idbalance;
+            if (value.name == balance.name &&
+                value.idbalance != balance.idbalance ) {
+                balance.name = balance.name + balance.idbalance;
             }
         });
-
-        // add new balance storage
-        this.addBalance(this.balance());
+        this.name.set(balance.name);
+        return balance;
     }
 
     addBalance(balance: Balance) {
@@ -78,6 +126,23 @@ export class BalanceStorage {
         this.balance_selection().selected.forEach((svalue, sindex) => {
             this.balance_dataToDisplay().forEach((value, index) => {
                 if (value.idbalance == svalue.idbalance) this.balance_dataToDisplay().splice(index, 1);
+            });
+        });
+        this.balance_dataSource().data = this.balance_dataToDisplay();
+        this.balance_selection().clear();
+        this.storageService.set(this.nmDataBalance, this.balance_dataToDisplay());
+        this.loadBalanceData();
+    }
+
+    editItem() {
+        this.balance_dataToDisplay.set(this.storageService.get(this.nmDataBalance));
+        this.balance_selection().selected.forEach((svalue, sindex) => {
+            this.balance_dataToDisplay().forEach((value, index) => {
+                if (value.idbalance == svalue.idbalance){
+                    this.idbalance.set(value.idbalance);
+                    this.name.set(value.name);
+                    this.free.set(value.free);
+                }
             });
         });
         this.balance_dataSource().data = this.balance_dataToDisplay();
